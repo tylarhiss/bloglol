6acc39d09f602a8f7a30595a319f636dc4d9d2dc
Tylar Hiss <tylarhiss@gmail.com>
2022-11-11 19:31:27 -0700

    spelling :(

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'B&R The BLog',
+  title: 'B&R The BLOG',
   tagline: 'Beers and Ruebens',
   url: 'https://zealous-dune-08ef2df1e.2.azurestaticapps.net',
   baseUrl: '/',
